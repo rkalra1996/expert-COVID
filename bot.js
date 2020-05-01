599fb4f5cea0ac33ddab84b5704a423457fd8295
rishabh kalra <rishabh.kalra@niit.com>
2020-05-01 15:12:31 +0530

    new bot added

---
+++ bot.js
@@ -0,0 +1,5 @@
+
+var s = document.createElement('script');
+s.type = 'text/javascript'; s.async = true;
+s.src = 'https://widget.botcopy.com/js/injection.js';
+document.getElementById('botcopy-embedder-d7lcfheammjct').appendChild(s);
